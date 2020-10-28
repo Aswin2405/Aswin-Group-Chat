e7bd527210ff591087ea33b40160671fd3306507
Aswin <69046983+Aswin2405@users.noreply.github.com>
2020-10-28 19:09:12 +0530

    changed

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -13,9 +13,20 @@ import FileCopyIcon from "@material-ui/icons/FileCopy";
 import ExpandLessIcon from "@material-ui/icons/ExpandLess";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import AddIcon from "@material-ui/icons/Add";
+// import db form "./firebase";
+import db from "./firebase";
 function Sidebar() {
   const [Channels, setChannels] = useState([]);
-  useEffect(() => {}, []);
+  useEffect(() => {
+    db.collection("rooms").onSnapshot((snapshot) =>
+      setChannels(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          name: doc.data().name
+        }))
+      )
+    );
+  }, []);
   return (
     <div className="sidebar">
       <div className="sidebar__header">
@@ -40,6 +51,9 @@ function Sidebar() {
       <SidebarOption Icon={ExpandMoreIcon} title="Channels" />
       <hr />
       <SidebarOption Icon={AddIcon} title="Add More Channels" />
+      {Channels.map((channel) => (
+        <SidebarOption title={channel.name} id={channel.id} />
+      ))}
     </div>
   );
 }
